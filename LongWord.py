829e32540da25e64bc126bb7b8e2664f794dcf29
m <murahf@gmail.com>
2020-06-08 08:23:04 +0300

    تم طرح هذا المشكلة مؤخرا من قبل Microsoft: السؤال: عندك متغير s يحمل قيمة نصية، جد أطول نص بدون تكرار الأحرف. هل يمكنك إيجاد الحل بوقت خطي (أي متناسب مع حجم النص (O(n ) مثال 1: s = 'Al-Khwarizmi' الناتج: 'Al-Khwarizm' مثال 2: s = 'Solution' الناتج: 'Soluti' مثال 3: 'Microsoft' الناتج: 'Micros' مثال 4: 'abcd' الناتج: 'abcd' مثال 5: 'abbcde' الناتج: 'bcde' السؤال الأصلي: Given a string, find the length of the longest substring without repeating characters. Can you find a solution in linear time?

--- LongWord.py
+++ LongWord.py
@@ -1,29 +1,29 @@
 def longWord(s):
-    s = s + '0'
-    s = list(s)
-    a = [""] * 128
-    repet = [False] * 128 # to check the reptation
-    temp = ''
-    long = 0  #length of longest word
-    x = 0  # index of longest word
+    repet=[-1]*(128+32)
+    temp=0
+    best_w_len=0
+    beg=0
+    s+=s[0]
+    s=list(s)
+    for i in range(len(s)):
+        if(repet[ord(s[i])]!=-1):
+            s[len(s)-1]=s[repet[ord(s[i])]+1]
+            if(temp>best_w_len):
+                best_w_len=temp
+                beg=i-best_w_len
+            temp=i-repet[ord(s[i])]-1
+            repet[ord(s[i])]=0
 
-    for i in s:
-        if (repet[ord(i)]):
-            repet = [False] * 128
-            if (len(temp) > len(a[ord(temp[0])])):
-                a[ord(temp[0])] = temp
-                if (long < len(temp)):
-                    x = ord(temp[0])
 
-                    long = len(temp)
-                    s[len(s) - 1] = i
-                temp = ''
+        temp+=1
+        repet[ord(s[i])]=i
+    best_w=''
+    for i in range(best_w_len):
+        best_w=best_w+s[beg]
+        beg+=1
+    return best_w
 
-        temp += i
-        repet[ord(i)] = True
-    return a[x]
 
 
 s = 'Microsoftopqrstuvwxyzabcd'
-word = longWord(s)
-print(word)
+print(longWord(s))
--- print.py
+++ print.py
@@ -1,1 +1,30 @@
-print("hello git hub")
+
+def longWord(s):
+    #repet = [False] * 128 # to check the reptation
+    w_len=0     #longest word length
+    last=''       #longest word start point
+    w_e=0        #longest word end
+    a=[0]*128
+    temp=1
+    for i in range(1,len(s)):
+        if(a[ord(s[i])]!=0):
+                w_e=ord(s[i-1])
+                print(s[i-1])
+                w_len=temp
+                temp=1
+
+
+        else:
+            a[ord(s[i])]=ord(s[i-1])
+            temp+=1
+    word=chr(w_e)
+    for i in range(w_len-1):
+        word=chr(a[w_e])+word
+        w_e=a[w_e]
+    return word
+
+
+
+s = 'Microsoftxyz'
+word = longWord(s)
+print(word)
